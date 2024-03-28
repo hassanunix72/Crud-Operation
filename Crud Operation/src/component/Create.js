ad4ec8264fcd61d31ff53493bc3925519f1bc861
hassanunix72 <163302823+hassanunix72@users.noreply.github.com>
2024-03-28 14:43:18 +0500

    crud app

---
+++ Crud Operation/src/component/Create.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Create = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    age: null,
+    isMarried: false,
+  });
+  const navigate = useNavigate();
+
+  const getData = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const sendData = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("https://66015d8587c91a11641aaeb4.mockapi.io/users", formData)
+      .then(() => {
+        alert("Data has been sent");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Failed to send data. Please try again later.");
+      });
+    e.target.reset();
+  };
+  const naviagation = () => {
+    navigate("/read");
+  };
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", mt: "40px" }}>
+      <form onSubmit={sendData}>
+        <Box sx={{ textAlign: "center" }}>
+          <Typography variant="h6">Enter Info</Typography>
+          <TextField
+            label="Enter Name"
+            name="name"
+            onChange={getData}
+            fullWidth
+            required
+            sx={{ margin: "10px 0" }}
+          />
+          <TextField
+            label="Enter Age"
+            name="age"
+            type="number"
+            onChange={getData}
+            fullWidth
+            required
+            sx={{ margin: "10px 0" }}
+          />
+          <FormControl fullWidth sx={{ margin: "10px 0" }}>
+            <InputLabel>Married ?</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Married"
+              name="isMarried"
+              value={formData.isMarried}
+              onChange={getData}>
+              <MenuItem value={true}>Yes</MenuItem>
+              <MenuItem value={false}>No</MenuItem>
+            </Select>
+          </FormControl>
+          <br />
+          <Button
+            variant="contained"
+            type="submit"
+            sx={{ margin: "10px 0", padding: " 5px 35px", width: "120px" }}>
+            Add
+          </Button>
+          <Button
+            variant="contained"
+            type="submit"
+            color="secondary"
+            onClick={naviagation}
+            sx={{ margin: "10px 5px", padding: " 5px 35px", width: "120px" }}>
+            Data
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  );
+};
+
+export default Create;
---
+++ Crud Operation/src/component/Read.js
@@ -0,0 +1,98 @@
+import { Box, Button, Grid, IconButton, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+const Read = () => {
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState([]);
+  const getData = () => {
+    axios
+      .get(`https://66015d8587c91a11641aaeb4.mockapi.io/users`)
+      .then((res) => {
+        setApiData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const editButton = (data) => {
+    navigate("/update", { state: { data } });
+    console.log(data);
+  };
+  const deleteButton = (id) => {
+    axios
+      .delete(`https://66015d8587c91a11641aaeb4.mockapi.io/users/${id}`)
+      .then(() => {
+        getData();
+      });
+  };
+  const navigateToAddData = () => {
+    navigate("/");
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      <Typography variant="h6">Data Collection</Typography>
+      {apiData.map((data, index) => {
+        return (
+          <Box
+            key={index}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}>
+            <Grid
+              container
+              maxWidth={"lg"}
+              alignContent="center"
+              sx={{
+                margin: "5px",
+                padding: "5px",
+                borderRadius: "5px",
+                boxShadow: 3,
+              }}>
+              <Grid item lg={1} sx={{ marginLeft: "15px" }}>
+                <Typography variant="h6">{data.id} : </Typography>
+              </Grid>
+              <Grid item lg={3}>
+                <Typography variant="h6">Name: {data.name}</Typography>
+              </Grid>
+              <Grid item lg={3}>
+                <Typography variant="h6">Age: {data.age}</Typography>
+              </Grid>
+              <Grid item lg={3}>
+                <Typography variant="h6">
+                  Married: {data.isMarried ? "Yes" : "No"}
+                </Typography>
+              </Grid>
+              <IconButton
+                sx={{ margin: "0 5px" }}
+                onClick={() => editButton(data)}>
+                <EditIcon color="primary" />
+              </IconButton>
+              <IconButton
+                sx={{ margin: "0 5px" }}
+                onClick={() => deleteButton(data.id)}>
+                <DeleteIcon color="error" />
+              </IconButton>
+            </Grid>
+          </Box>
+        );
+      })}
+      <Button
+        onClick={navigateToAddData}
+        variant="contained"
+        color="secondary"
+        sx={{ margin: "10px", width: "62%" }}>
+        Add Data
+      </Button>
+    </Box>
+  );
+};
+
+export default Read;
